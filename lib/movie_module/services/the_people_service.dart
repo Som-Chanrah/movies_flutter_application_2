7d291feb91b3c238426ae6c769559903e1b17619
Som Chanrah <chanrahsom@gmail.com>
2026-06-15 22:48:37 +0700

    Update the_people_service.dart

--- lib/movie_module/services/the_people_service.dart
+++ lib/movie_module/services/the_people_service.dart
@@ -1,28 +1,39 @@
-import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
+import '../api_key.dart';
 import '../models/person_model.dart';
 
 class ThePeopleService {
+  final baseUrl = "https://api.themoviedb.org/3/person";
+
   Future<PopularPeople> readPopular() async {
-    await Future.delayed(Duration(milliseconds: 200));
-    final results = List.generate(
-      8,
-      (i) =>
-          PersonSummary(id: i + 1, name: 'Person ${i + 1}', profilePath: null),
-    );
-    return PopularPeople(results: results);
+    try {
+      http.Response response = await http.get(
+        Uri.parse("$baseUrl/popular?language=en-US&page=1&api_key=$apiKey"),
+      );
+      if (response.statusCode == 200) {
+        return compute(popularPeopleFromJson, response.body);
+      } else {
+        throw Exception("Error status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
-  Future<PersonDetail> get(String id) async {
-    await Future.delayed(Duration(milliseconds: 200));
-    final intId = int.tryParse(id) ?? 0;
-    return PersonDetail(
-      id: intId,
-      name: 'Person $id',
-      profilePath: null,
-      biography: 'Biography for person $id',
-      birthday: '1970-01-01',
-      placeOfBirth: 'Unknown',
-    );
+  Future<PersonDetail> get(String personId) async {
+    try {
+      http.Response response = await http.get(
+        Uri.parse("$baseUrl/$personId?language=en-US&api_key=$apiKey"),
+      );
+      if (response.statusCode == 200) {
+        return compute(personDetailFromJson, response.body);
+      } else {
+        throw Exception("Error status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
